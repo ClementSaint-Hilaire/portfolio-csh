c6a0cf9e607d274fcb269b4a1b17e0b3ab0fd038
csh <clement.sainthilaire.pro@gmail.com>
2025-01-03 14:27:28 +0100

    update des titres

--- src/app/icons/page.tsx
+++ src/app/icons/page.tsx
@@ -15,17 +15,12 @@ export default function Page() {
         <section id="hero" className="flex min-h-[25dvh] flex-1 items-center justify-center text-center">
             <div className="w-full max-w-2xl mx-auto">
                 <div className="flex flex-col items-center justify-centers">
-                <BlurFadeText
-                  delay={BLUR_FADE_DELAY}
-                  className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300 bg-clip-text text-center text-4xl lg:text-5xl font-semibold leading-none text-transparent dark:from-white dark:to-black"
-                  yOffset={8}
-                  text={`csh Icons`}
-                />
-                  <BlurFadeText
-                  className="max-w-[700px] text-2xl text-gris_2"
-                  delay={BLUR_FADE_DELAY}
-                    text={`Juste ce dont vous avez besoin`}
-                  />
+                <BlurFade>
+                  <div className="w-full lg:w-max text-center"><span className="text-noir text-3xl lg:text-5xl font-bold uppercase lg:leading-[88px] dark:text-blanc">csh</span><span className="text-[#e1e1e1] text-3xl lg:text-5xl font-bold uppercase lg:leading-[88px] dark:text-gris_2"> Icons</span></div>
+                </BlurFade>
+                <BlurFade>
+                  <div className="grow shrink basis-0 text-center text-[#75757a] text-[17px] font-normal lg:text-[21px] font-normal leading-7">Just what you need</div>
+                </BlurFade>
                 </div>
             </div>
         </section>
